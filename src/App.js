e61044fd6a801d7ebd57255535932d334ebf96da
Kevin-Lejava <klejava@gmail.com>
2022-01-12 19:41:49 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { Fragment } from 'react';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [city, setCity] = useState("");
+  const [data, setData] = useState([]);
+  console.log(city);
+
+  const getCities = (event, city) => {
+    event.preventDefault();
+    fetch(`http://ctp-zip-api.herokuapp.com/city/${city}`)
+      .then((response) => {
+        console.log(response)
+        return response.json();
+      }).then((data) => {
+        console.log(data)
+        setData(data);
+      }).catch((err) => {
+        console.error(err)
+      })
+  }
+
+  console.log(data);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Fragment>
+          <form>
+            <label>
+              City Name:
+              <input type="text" name="city" onChange={(e) => setCity(e.target.value)} />
+            </label>
+            <input type="submit" value="Submit" onClick={(e) => getCities(e, city)} />
+          </form>
+        </Fragment>
+
+        <ul className="ListGroup">
+
+          {data.map((element) => {
+            return (
+              <li className="list-group-item">{element}</li>
+            )
+          })}
+
+        </ul>
       </header>
+
+
+
     </div>
   );
 }
